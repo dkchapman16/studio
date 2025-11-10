790f86e609f250516a544dff3460bf4a1bdab47b
Devin Chapman <devinkchapman@hitchedlogistics.com>
2025-11-10 23:19:52 +0000

    not fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <SidebarProvider>
           <div className="flex min-h-screen w-full flex-col">
             <AppSidebar />
-            <div className="flex flex-col sm:gap-4 sm:pl-14">
+            <div className="flex flex-col sm:pl-14">
               <Header />
               <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
                 {children}
